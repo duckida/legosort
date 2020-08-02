b4c882251c0ae217e1bb046ac5911009a1cfa362
Duckida <61168649+duckida@users.noreply.github.com>
2020-08-02 16:21:04 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -14,10 +14,6 @@ minpw = (1.0-correction)/1000
 btm = Servo(17, min_pulse_width=minpw, max_pulse_width=maxpw)
 top = Servo(26, min_pulse_width=minpw, max_pulse_width=maxpw)
 
-
-
-
-
 def setup():
   GPIO.setmode(GPIO.BCM)
   GPIO.setup(signal,GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -39,10 +35,9 @@ def loop():
     start = time.time()
     for impulse_count in range(NUM_CYCLES):
       GPIO.wait_for_edge(signal, GPIO.FALLING)
-    duration = time.time() - start      #seconds to run for loop
-    red  = NUM_CYCLES / duration   #in Hz
-    print("red value - ",red)
-
+    duration = time.time() - start 
+    red  = NUM_CYCLES / duration   
+   
     GPIO.output(s2,GPIO.LOW)
     GPIO.output(s3,GPIO.HIGH)
     time.sleep(0.3)
@@ -51,7 +46,7 @@ def loop():
       GPIO.wait_for_edge(signal, GPIO.FALLING)
     duration = time.time() - start
     blue = NUM_CYCLES / duration
-    print("blue value - ",blue)
+    
 
     GPIO.output(s2,GPIO.HIGH)
     GPIO.output(s3,GPIO.HIGH)
@@ -61,45 +56,20 @@ def loop():
       GPIO.wait_for_edge(signal, GPIO.FALLING)
     duration = time.time() - start
     green = NUM_CYCLES / duration
-    print("green value - ",green)
-    time.sleep(2)
     
-    if green > 12500:
-        print("Green")
-        print("Box 2")
-        time.sleep(1)
-        top.mid()
-        print("Dropping into Box")
-        time.sleep(1)
-        top.min()
-        print("going back")
-        time.sleep(1)
-        btm.mid()
-        
-    elif red > 12500:
-        print("Red")
-        top.min()
-        btm.min()
-        print("Box 1")
-        time.sleep(1)
-        top.mid()
-        print("Dropping into Box")
-        time.sleep(1)
-        top.min()
-        print("going back")
-        time.sleep(1)
-        btm.mid()
-        
-    elif blue > 12500:
-        print("Blue")
-        print("Box 3")
-        time.sleep(1)
-        top.mid()
-        print("Dropping into Box")
-        time.sleep(1)
-        top.min()
-        print("going back")
-        time.sleep(1)
+      
+    if green<7000 and blue<7000 and red>12000:
+      print("Red")
+      temp=1
+    elif red<12000 and  blue<12000 and green>12000:
+      print("Green")
+      temp=1
+    elif green<7000 and red<7000 and blue>12000:
+      print("Blue")
+      temp=1
+    elif red>10000 and green>10000 and blue>10000 and temp==1:
+      print("Waiting for Brick.....")
+      temp=0
 
 
 def endprogram():
@@ -114,5 +84,3 @@ if __name__=='__main__':
 
     except KeyboardInterrupt:
         endprogram()
-
- 
